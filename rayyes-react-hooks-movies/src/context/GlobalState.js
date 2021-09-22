7f3055702dcaa3d4c2349bb9adab535050e8c9d2
modrayyes <mod.rayyes@gmail.com>
2021-09-22 20:07:09 +0300

    react-hooks movie app (useState -useEffect - useContext)

---
+++ rayyes-react-hooks-movies/src/context/GlobalState.js
@@ -0,0 +1,39 @@
+import React, { createContext, useEffect, useState } from "react";
+
+// initial state
+const initialState = {
+  Favourites: localStorage.getItem("Favourites")
+    ? JSON.parse(localStorage.getItem("Favourites"))
+    : [],
+};
+// create context
+export const GlobalContext = createContext(initialState);
+
+// provider components
+export const GlobalProvider = (props) => {
+ 
+  const [state, setState] = useState(initialState);
+ 
+
+  useEffect(() => {
+    localStorage.setItem("Favourites", JSON.stringify(state.Favourites || []));
+  }, [state]);
+
+  // action
+  const addMovieToFavourites = (movie) => {
+    setState((prevState) => {
+      console.log({prevState})
+      return  {Favourites:[...prevState.Favourites, movie]}});
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        Favourites: state.Favourites,
+        addMovieToFavourites,
+      }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
